65183172dd9d71d3a079a4763e54c22bbf64c749
3cha9 <yoshq.xd@gmail.com>
2023-05-09 15:14:21 +0100

    initial

---
+++ src/config.mjs
@@ -0,0 +1,35 @@
+import defaultImage from './assets/images/default.png';
+
+const CONFIG = {
+  name: 'Swippro',
+
+  origin: 'https://astrowind.vercel.app',
+  basePathname: '/',
+  trailingSlash: false,
+
+  title: 'AstroWind — Free template for create a website with Astro + Tailwind CSS',
+  description:
+    '🚀 Suitable for Startups, Small Business, Sass Websites, Professional Portfolios, Marketing Websites, Landing Pages & Blogs.',
+  defaultImage: defaultImage,
+
+  defaultTheme: 'system', // Values: "system" | "light" | "dark" | "light:only" | "dark:only"
+
+  language: 'en',
+  textDirection: 'ltr',
+
+  dateFormatter: new Intl.DateTimeFormat('en', {
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+    timeZone: 'UTC',
+  }),
+
+  googleAnalyticsId: false, // or "G-XXXXXXXXXX",
+  googleSiteVerificationId: 'orcPxI47GSa-cRvY11tUe6iGg2IO_RPvnA1q95iEM3M',
+
+
+};
+
+export const SITE = { ...CONFIG, blog: undefined };
+export const BLOG = CONFIG.blog;
+export const DATE_FORMATTER = CONFIG.dateFormatter;
---
+++ src/data.js
@@ -0,0 +1,40 @@
+import { getPermalink, getAsset } from './utils/permalinks';
+
+export const headerData = {
+  links: [
+  
+    {
+      text: 'Pricing',
+      href: '#',
+    },
+    {
+      text: 'Features',
+      href: '#',
+    },
+    {
+      text: 'About',
+      href: '#',
+    },
+  ],
+  actions: [
+    { type: 'button', text: 'Download', href: '#' }
+  ],
+};
+  
+export const footerData = {
+  
+  secondaryLinks: [
+    
+    { text: 'About', href: getPermalink('') },
+    { text: 'Terms', href: getPermalink('/terms') },
+    { text: 'Privacy', href: getPermalink('/privacy') },
+    { text: 'Contact', href: getPermalink('') }
+    
+  ],
+  socialLinks: [
+    { ariaLabel: 'LinkedIn', icon: 'tabler:brand-linkedin', href: '#' },
+    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
+    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
+    { ariaLabel: 'Twitter', icon: 'tabler:brand-twitter', href: '' },
+  ],
+};
